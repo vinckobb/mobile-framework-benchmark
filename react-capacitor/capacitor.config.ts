5481be6098bc68afe7c17696a603e89930ac8f29
tomas.vince <tomas.vince@asseco-ce.com>
2025-03-14 13:44:59 +0100

    Added android platform

---
+++ react-capacitor/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vinckobb.mobile.framework.benchmark',
+  appName: 'react-capacitor',
+  webDir: 'out'
+};
+
+export default config;
